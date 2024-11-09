5ce2288bbf651270d05bc18737eadd174eb402b0
sanjit <sanjitkeleng5@gmai.com>
2024-11-08 20:12:23 -0500

    commit

--- lib/SharedPreferences/SharedPreferences.dart
+++ lib/SharedPreferences/SharedPreferences.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -26,24 +25,16 @@ class _HomePageState extends State<HomePage> {
 
   var nameController=TextEditingController();
 
-  //result:
   String name='';
   @override
 
-  //app open huar loge loge get hoi jai
   void initState() {
     super.initState();
-
-    //nije function bonai
     getValueFromPrefs();
-
-    ///value fect
   }
   void getValueFromPrefs()async{
     SharedPreferences prefs=await SharedPreferences.getInstance();
 
-    //if null =key judi corect nhoi aru key judi value get hua nai tatia
-    //null dekha dibo blank value ,judi value ahi jai tatia value get hobo
     name=prefs.getString('name') ??'';
     setState(() {});
   }
@@ -59,9 +50,6 @@ class _HomePageState extends State<HomePage> {
       body: Column(
         children: [
 
-          //shoew value:
-          //not equal blank mane judi blank equal nhoi tatia name ahi jabo ,
-          //judi blank equal hoi blank show kuribo
           Text(name !=  ''?  'Welcom , $name' :""),
 
           TextField(
@@ -71,20 +59,12 @@ class _HomePageState extends State<HomePage> {
 
           Divider(),
 
-
-          //Async okl nije bona functiont he binabo pare:
           ElevatedButton(onPressed: ()async{
-
-            //prefernc kam hoingl :
             SharedPreferences prefs= await  SharedPreferences.getInstance();
-
-            //prefrence ki store kurino khitu mantion :
             prefs.setString('name', nameController.text);
           },
-              //value store
+
               child: Text('get value'))
-
-
         ],
       ),
     );
